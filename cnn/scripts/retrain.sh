fbae79d3ce125b5da6be76a85d4ecbed643220d8
Yi-Wei Chen <feberium@gmail.com>
2019-09-21 16:43:31 -0500

    Update genotypes and retrain

--- cnn/scripts/retrain.sh
+++ cnn/scripts/retrain.sh
@@ -205,14 +205,42 @@ SEED=1989
 ##    --dataset cifar10 --corruption_prob ${ETA} --corruption_type unif --gold_fraction 0 --loss_func ${LOSS} \
 ##    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}
 
+#LOSS="rll"
+#ARCH="DARTS_V2"
+#GPU=3
+#ALPHA=0.01
+#EPOCHS=600
+#LAYERS=20
+#ETA=0.6
+#EXP_PATH="exp/DARTS/unif_${ETA}_cifar10_seed${SEED}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+#
+#python train.py --data cifar10 --batch_size 64 --gpu ${GPU} \
+#    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --auxiliary --cutout \
+#    --dataset cifar10 --corruption_prob ${ETA} --corruption_type unif --gold_fraction 0 --loss_func ${LOSS} \
+#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}
+
+#LOSS="rll"
+#ARCH="RLL001_UNIFORM_06_1989"
+#GPU=3
+#ALPHA=0.01
+#EPOCHS=600
+#LAYERS=20
+#ETA=0.6
+#EXP_PATH="exp/MyDARTS/unif_${ETA}_cifar10_seed${SEED}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+#
+#python train.py --data cifar10 --batch_size 64 --gpu ${GPU} \
+#    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --auxiliary --cutout \
+#    --dataset cifar10 --corruption_prob ${ETA} --corruption_type unif --gold_fraction 0 --loss_func ${LOSS} \
+#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}
+
 LOSS="rll"
-ARCH="DARTS_V2"
-GPU=3
+ARCH="CCE_UNIFORM_06_SEED_1989"
+GPU=0
 ALPHA=0.01
 EPOCHS=600
 LAYERS=20
 ETA=0.6
-EXP_PATH="exp/DARTS/unif_${ETA}_cifar10_seed${SEED}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+EXP_PATH="exp/MyDARTS/unif_${ETA}_cifar10_seed${SEED}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
 
 python train.py --data cifar10 --batch_size 64 --gpu ${GPU} \
     --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --auxiliary --cutout \
