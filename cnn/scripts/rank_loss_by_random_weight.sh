e625928d474372a18e65e6349e8dde324838dd50
Yi-Wei Chen <feberium@gmail.com>
2019-10-04 01:09:48 -0500

    Update rank_loss_by_random_weight.sh

--- cnn/scripts/rank_loss_by_random_weight.sh
+++ cnn/scripts/rank_loss_by_random_weight.sh
@@ -17,34 +17,34 @@ DATA="cifar10"
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
 #    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}  --random_weight
 
-LOSS="rll"
-ARCH="RLL001_UNIFORM_06"
-ALPHA=0.01
-EPOCHS=3500
-LAYERS=8
-ETA=0.6
-GPU=0
-EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F2_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
-
-python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
-    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
-    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}  --random_weight
-
-
 #LOSS="rll"
-#ARCH="CCE_UNIFORM_06"
+#ARCH="RLL001_UNIFORM_06"
 #ALPHA=0.01
 #EPOCHS=3500
 #LAYERS=8
 #ETA=0.6
 #GPU=0
-#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/no_aux_no_dropout/F3_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F2_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
 #
 #python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}
+#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}  --random_weight
+
+
+LOSS="rll"
+ARCH="CCE_UNIFORM_06"
+ALPHA=0.01
+EPOCHS=3500
+LAYERS=8
+ETA=0.6
+GPU=2
+EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F3_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+
+python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
+    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
+    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
+    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}   --random_weight
 
 
 #LOSS="cce"
@@ -57,7 +57,7 @@ python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}
+#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}   --random_weight
 
 #LOSS="cce"
 #ARCH="RLL001_UNIFORM_06"
@@ -65,12 +65,12 @@ python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #LAYERS=8
 #ETA=0.6
 #GPU=1
-#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/no_aux_no_dropout/F2_unif${ETA}_${DATA}_${LOSS}_${ARCH}_gpu${GPU}"
+#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F2_unif${ETA}_${DATA}_${LOSS}_${ARCH}_gpu${GPU}"
 #
 #python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED}  --cutout --drop_path_prob 0 \
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}
+#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}   --random_weight
 
 #LOSS="rll"
 #ARCH="CCE_UNIFORM_06"
@@ -79,12 +79,12 @@ python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #LAYERS=8
 #ETA=0.6
 #GPU=0
-#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/no_aux_no_dropout/F3_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
+#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F3_unif${ETA}_${DATA}_${LOSS}${ALPHA}_${ARCH}_gpu${GPU}"
 #
 #python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}
+#    --arch ${ARCH} --alpha ${ALPHA} --train_portion 0.9 --layers ${LAYERS}   --random_weight
 #
 #LOSS="cce"
 #ARCH="CCE_UNIFORM_06"
@@ -92,12 +92,12 @@ python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #LAYERS=8
 #ETA=0.6
 #GPU=2
-#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/no_aux_no_dropout/F3_unif${ETA}_${DATA}_${LOSS}_${ARCH}_gpu${GPU}"
+#EXP_PATH="exp/MyDARTS/rank_loss_seed${SEED}/random_weight/F3_unif${ETA}_${DATA}_${LOSS}_${ARCH}_gpu${GPU}"
 #
 #python rank_loss.py --data "/home/yiwei/${DATA}" --batch_size 64 --gpu ${GPU} \
 #    --epochs ${EPOCHS} --save ${EXP_PATH} --seed ${SEED} --cutout --drop_path_prob 0 \
 #    --corruption_prob ${ETA} --corruption_type unif --loss_func ${LOSS} \
-#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}
+#    --arch ${ARCH} --train_portion 0.9 --layers ${LAYERS}   --random_weight
 
 
 #LOSS="rll"
